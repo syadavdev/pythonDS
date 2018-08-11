56c773c7183f396ff9994255e1571404abce0fa4
Sandeep <sandeep.yadav@noosyntech.in>
2018-08-12 01:47:20 +0530

    Solve Max Sub Array Sum Problem

---
+++ app/array/max_sub_array.py
@@ -0,0 +1,23 @@
+"""
+input:
+1st row(array) : -2 -4 3 -1  6 -3 4
+output: 10
+"""
+
+
+def find_max_subarray_sum(arry, length):
+    max = -1000000
+    sum = 0
+    for i in range(0, length):
+        sum = sum + arry[i]
+        if sum > max:
+            max = sum
+        if sum < 0:
+            sum = 0
+    return max
+
+
+if __name__ == "__main__":
+    arry = list(map(int, input().split()))
+    print(arry)
+    print(find_max_subarray_sum(arry, len(arry)))
