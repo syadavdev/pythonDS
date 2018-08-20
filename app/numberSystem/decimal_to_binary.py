9db7c1f58862ee5b7f8fe2d5414527f7ffb2264d
Sandeep <sandeep.yadav@noosyntech.in>
2018-08-20 23:31:55 +0530

    add up another fudecimal to binary function

--- app/numberSystem/decimal_to_binary.py
+++ app/numberSystem/decimal_to_binary.py
@@ -12,7 +12,24 @@ def decimal_to_binary(num):
     else:
         return num % 2 + 10 * decimal_to_binary(int(num/2))
 
+sum = 0
+factor = 1
+
+
+def decimal_to_binary2(num):
+    global sum
+    global factor
+    if num == 0:
+        return 0
+    else:
+        reminder = num % 2
+        sum = sum + reminder * factor
+        factor = factor * 10
+        decimal_to_binary2(int(num/2))
+        return sum
+
 
 if __name__ == "__main__":
     number = int(input())
     print(decimal_to_binary(number))
+    print(decimal_to_binary2(number))
