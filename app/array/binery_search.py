8ad3b54a2491706d4da2a525ade8fadac3f475cc
Sandeep <sandeep.yadav@noosyntech.in>
2018-08-23 02:51:55 +0530

    binary search algo

---
+++ app/array/binery_search.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+def binery_search(arry ,low, high, ele):
+    mid = (low+high)//2
+    if low <= high:
+        if arry[mid] == ele:
+            return mid+1
+        elif arry[mid] > ele:
+            return binery_search(arry, low, mid-1, ele)
+        return binery_search(arry, mid+1, high, ele)
+    else:
+        return -1
+
+
+class BinarySearchTestCase(unittest.TestCase):
+
+    def test_cross(self):
+        self.assertEqual(binery_search([3 ,4, 5, 6, 7, 8, 9], 0, 6, 4), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
