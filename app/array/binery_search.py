2c279d98923b86f844bf423cb9a67778360217dd
Sandeep Yadav <sandi10.96@gmail.com>
2018-08-23 15:24:57 +0530

    Write Second test case for binery search

--- app/array/binery_search.py
+++ app/array/binery_search.py
@@ -15,8 +15,11 @@ def binery_search(arry ,low, high, ele):
 
 class BinarySearchTestCase(unittest.TestCase):
 
-    def test_binery_search(self):
+    def test_binery_search_one(self):
         self.assertEqual(binery_search([3 ,4, 5, 6, 7, 8, 9], 0, 6, 4), 2)
+
+    def test_binery_search_two(self):
+        self.assertEqual(binery_search([3, 4, 5, 6, 7, 8, 9], 0, 6, 1), -1)
 
 
 if __name__ == "__main__":
