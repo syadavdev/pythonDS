e757f95b0c5bb257b635103c429496266f09e49b
Sandeep Yadav <sandi10.96@gmail.com>
2018-08-24 14:45:17 +0530

    change app to src folder

--- src/matrix/matix _multiplication.py
+++ src/matrix/matix _multiplication.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-def multiplication(A, B):
+def multiplication_one(A, B):
     n = len(A)
     C = [[0 for x in range(0, n)] for y in range(0, n)]
     for i in range(0, n):
@@ -12,14 +12,21 @@ def multiplication(A, B):
     return C
 
 
+def multiplication_two(A, B):
+    pass
+
+
 class MultiplicationTest(unittest.TestCase):
 
     A = [[1,2,3] ,[4,5,6], [7,8,9]]
     B = [[1,2,3] ,[4,5,6], [7,8,9]]
     C = [[30, 36, 42], [66, 81, 96], [102, 126, 150]]
 
-    def test_multiplcation(self):
-        self.assertEqual(multiplication(MultiplicationTest.A ,MultiplicationTest.B), MultiplicationTest.C)
+    def test_multiplication_one(self):
+        self.assertEqual(multiplication_one(MultiplicationTest.A ,MultiplicationTest.B), MultiplicationTest.C)
+
+    def test_multiplication_two(self):
+        self.assertEqual(multiplication_two(MultiplicationTest.A, MultiplicationTest.B), MultiplicationTest.C)
 
 
 if __name__ == "__main__":
